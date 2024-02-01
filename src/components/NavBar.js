1f2b6a4c243e8f670d3d7a2f10ded2e2245b0234
stellamumbi <stellamumbi736@gmail.com>
2024-02-02 00:55:35 +0300

    completed lab

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,15 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  // Mapping the links to create <a> elements
+  const linkElements = links.map((link) => (
+    <a key={link} href={`#${link}`}>
+      {link}
+    </a>
+  ));
+
+  // Returning the navigation bar with the mapped links
+  return <nav>{linkElements}</nav>;
 }
 
 export default NavBar;
--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,15 +1,20 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
+
+  const projectTech = technologies.map((tech) => {
+    return <span key={tech}>{tech}</span>
+  })
+
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
         {/* render a <span> for each technology in the technologies array */}
+        {projectTech}
       </div>
     </div>
   );
 }
-
 export default ProjectItem;
